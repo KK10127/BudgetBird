15f35340fe3ee16df9b347591b90c99f449acebb
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-09 16:00:53 -0700

    - signup activiy xml transferred over and java file created

---
+++ app/src/main/java/edu/miracosta/financialassistant/SignUpActivity.java
@@ -0,0 +1,16 @@
+package edu.miracosta.financialassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Resposible for handling the signup process for the application.
+ */
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
