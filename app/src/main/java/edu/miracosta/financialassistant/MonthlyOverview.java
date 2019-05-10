263f80a48c96ecceb8a1280de5e7b94f633bbf3b
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-10 15:55:29 -0700

    - monthly overview xml design complete - added table 3rd party library - set up pie-chart/table in MonthlyOverviewActivity.java - Set up intents/activity changes in MainActivity.java

--- app/src/main/java/edu/miracosta/financialassistant/MonthlyOverview.java
+++ app/src/main/java/edu/miracosta/financialassistant/MonthlyOverview.java
@@ -1,13 +1,42 @@
 package edu.miracosta.financialassistant;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
+
+/**
+ * Activity is responsible for displaying the user's monthly overview.
+ * This will consist of a pie chart and a few data indicators. This activity is essentially
+ * the dashboard of the entire app.
+ */
 public class MonthlyOverview extends AppCompatActivity {
+
+    PieChartView mPieChartView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_overview);
+
+
+        mPieChartView = findViewById(R.id.chart);
+        List<SliceValue> pieData = new ArrayList<>();
+
+        pieData.add(new SliceValue(15, Color.BLUE));
+        pieData.add(new SliceValue(25, Color.GRAY));
+        pieData.add(new SliceValue(10, Color.RED));
+        pieData.add(new SliceValue(60, Color.MAGENTA));
+
+        PieChartData pieChartData = new PieChartData(pieData);
+
+        mPieChartView.setPieChartData(pieChartData);
+
     }
 }
