4eb4cbba789439e0127cd5b2690ba6b58e1f6d1c
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-12 15:37:47 -0700

    - MonthlyOverview.java skeleton complete and TODO's set up

--- app/src/main/java/edu/miracosta/financialassistant/MonthlyOverview.java
+++ app/src/main/java/edu/miracosta/financialassistant/MonthlyOverview.java
@@ -1,8 +1,12 @@
 package edu.miracosta.financialassistant;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,28 +19,90 @@ import lecho.lib.hellocharts.view.PieChartView;
  * Activity is responsible for displaying the user's monthly overview.
  * This will consist of a pie chart and a few data indicators. This activity is essentially
  * the dashboard of the entire app.
+ *
+ * The following needs to be accomplished in this activity each time it starts:
+ *
+ * 1) Upon the activity starting, the user's current monthly budget must be updated by using the
+ *      database.
+ * 2) Additionally, the users monthly expenses should be retrieved and used with the current month's
+ *      budget to update the PieChartView. Details on using the PieChartView are below
+ * 2) The user's amount of available emergency funding is also retrieved from the database and the
+ *      emergencyFundTextView is updates.
+ *
+ *
  */
 public class MonthlyOverview extends AppCompatActivity {
 
-    PieChartView mPieChartView;
+    // define all interacting components
+    private PieChartView pieChartView;
+    private TextView budgetTextView;
+    private Button todayButton;
+    private Button trendsButton;
+    private TextView emergencyFundTextView;
+
+    // TODO: More variables may need to be defined
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_overview);
 
+        // wire up necessary components
+        pieChartView = findViewById(R.id.pieChartView);
+        budgetTextView = findViewById(R.id.budgetTextView);
+        todayButton = findViewById(R.id.todayButton);
+        trendsButton = findViewById(R.id.trendsButton);
+        emergencyFundTextView = findViewById(R.id.emergencyFundTextView);
 
-        mPieChartView = findViewById(R.id.chart);
+        // create slices list for the pieData
         List<SliceValue> pieData = new ArrayList<>();
 
+        // test slices
         pieData.add(new SliceValue(15, Color.BLUE));
         pieData.add(new SliceValue(25, Color.GRAY));
         pieData.add(new SliceValue(10, Color.RED));
         pieData.add(new SliceValue(60, Color.MAGENTA));
 
+        // create pieChartData using the list of pie slices
         PieChartData pieChartData = new PieChartData(pieData);
+        // set the pieCharData to update the pieChart
+        pieChartView.setPieChartData(pieChartData);
 
-        mPieChartView.setPieChartData(pieChartData);
+        // TODO: gather needed details from the database
+        // TODO: The user's monthly budget, monthly expenses, emergency fund amount
+
+
+        // TODO: update the chart update views accordingly
+
+
 
     }
+
+    /**
+     * This method takes users to the 'Today' activity
+     * @param v
+     */
+    public void todayClicked(View v) {
+        Intent intent = new Intent(this, TodayActivity.class);
+        startActivity(intent);
+
+        // TODO: Decide if any data needs to be sent to TodayActivity.java
+        // TODO: Update this block if necessary.
+
+    }
+
+    /**
+     * This method takes users to the 'Trends' activity
+     * @param v
+     */
+    public void trendsClicked(View v) {
+        Intent intent = new Intent(this, TrendsActivity.class);
+        startActivity(intent);
+
+        // TODO: Decide if any data needs to be sent to TrendsActivity.java
+        // TODO: Update this block if necessary
+    }
+
+
 }
