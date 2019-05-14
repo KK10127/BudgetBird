036de7ab2bbafd4091f3eaa5bd9f15c4758ac3a6
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-14 14:11:25 -0700

    - fixed it

--- app/src/main/java/edu/miracosta/financialassistant/MonthlyOverview.java
+++ app/src/main/java/edu/miracosta/financialassistant/MonthlyOverview.java
@@ -40,8 +40,7 @@ import lecho.lib.hellocharts.view.PieChartView;
  *
  *
  */
-public class MonthlyOverview extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+public class MonthlyOverview extends AppCompatActivity {
 
     // define all interacting components
     private PieChartView pieChartView;
@@ -59,7 +58,7 @@ public class MonthlyOverview extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.body);
+        setContentView(R.layout.activity_monthly_overview);
 
         // wire up necessary components
         pieChartView = findViewById(R.id.pieChartView);
@@ -89,28 +88,6 @@ public class MonthlyOverview extends AppCompatActivity
         // TODO: update the chart update views accordingly
 
 
-        //**********************************
-        Toolbar toolbar = (Toolbar) findViewById(R.id.nav_view);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
 
     }
 
@@ -138,63 +115,5 @@ public class MonthlyOverview extends AppCompatActivity
         // TODO: Decide if any data needs to be sent to TrendsActivity.java
         // TODO: Update this block if necessary
     }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.monthy_overview2, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
 
 }
