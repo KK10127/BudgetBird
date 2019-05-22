ba489a59fdc0dc6d3e66250a4224998bec122ba4
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-22 02:29:37 -0700

    added animation to splash screen

--- app/src/main/java/edu/miracosta/financialassistant/SplashScreen.java
+++ app/src/main/java/edu/miracosta/financialassistant/SplashScreen.java
@@ -3,16 +3,25 @@ package edu.miracosta.financialassistant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashScreen extends AppCompatActivity {
 
+    RotateAnimation r;
+    private ImageView logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         //Delay the activity with a TimerTask
         TimerTask task = new TimerTask() {
             @Override
@@ -24,7 +33,15 @@ public class SplashScreen extends AppCompatActivity {
             }
         };
         Timer timer = new Timer();
-        timer.schedule(task, 4000);
+
+        logo = findViewById(R.id.logoImageView);
+
+        r = new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        r.setDuration((long) 600);
+        r.setRepeatCount(2);
+        logo.startAnimation(r);
+
+        timer.schedule(task, 2500);
 
         //Can load things in background here
     }
