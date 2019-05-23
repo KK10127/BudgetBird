b424a39f2ce68eebdfe1943765624331b709423f
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-23 01:46:29 -0700

    finished IncomeTest.java

--- app/src/androidTest/java/edu/miracosta/financialassistant/model/IncomeTest.java
+++ app/src/androidTest/java/edu/miracosta/financialassistant/model/IncomeTest.java
@@ -8,8 +8,11 @@ import static org.junit.Assert.*;
 
 public class IncomeTest {
 
+    private Income testIncome;
+
     @Before
     public void setUp() throws Exception {
+        testIncome = new Income(2500, "My 2 bedroom apartment", "Rent");
     }
 
     @After
@@ -18,41 +21,43 @@ public class IncomeTest {
 
     @Test
     public void getIncomeName() {
+        assertEquals("Testing the getIncomeName() Method.", "Rent", testIncome.getIncomeName());
     }
 
     @Test
     public void setIncomeName() {
+        testIncome.setIncomeName("Rent Rent!");
+        assertEquals("Testing the setIncomeName() Method.", "Rent Rent!", testIncome.getIncomeName());
     }
 
     @Test
     public void getIncomeValue() {
+        assertEquals("Testing the getIncomeValue() Method.", String.valueOf(2500), String.valueOf(testIncome.getIncomeValue()));
     }
 
     @Test
     public void setIncomeValue() {
+        testIncome.setIncomeValue(500);
+        assertEquals("Testing setIncomeValue()!", String.valueOf(500), String.valueOf(testIncome.getIncomeValue()));
     }
 
     @Test
     public void setId() {
+        testIncome.setId(-1);
+        assertEquals("Testing setId()!", String.valueOf(-1), String.valueOf(testIncome.getId()));
     }
 
     @Test
     public void getId() {
+        assertEquals("Testing the getId() Method.", String.valueOf(0) , String.valueOf(testIncome.getId()));
+
     }
 
     @Test
     public void getIncomeDesc() {
+        assertEquals("Testing the getIncomeDesc() Method.", "My 2 bedroom apartment" , testIncome.getIncomeDesc());
+
     }
 
-    @Test
-    public void setIncomeDesc() {
-    }
 
-    @Test
-    public void describeContents() {
-    }
-
-    @Test
-    public void writeToParcel() {
-    }
 }
