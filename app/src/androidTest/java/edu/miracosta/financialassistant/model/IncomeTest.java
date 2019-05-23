412582d5a0455c2976ce1350682cdb5dc7bfbb4b
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-23 01:28:02 -0700

    created IncomeTest.java

---
+++ app/src/androidTest/java/edu/miracosta/financialassistant/model/IncomeTest.java
@@ -0,0 +1,58 @@
+package edu.miracosta.financialassistant.model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IncomeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getIncomeName() {
+    }
+
+    @Test
+    public void setIncomeName() {
+    }
+
+    @Test
+    public void getIncomeValue() {
+    }
+
+    @Test
+    public void setIncomeValue() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getIncomeDesc() {
+    }
+
+    @Test
+    public void setIncomeDesc() {
+    }
+
+    @Test
+    public void describeContents() {
+    }
+
+    @Test
+    public void writeToParcel() {
+    }
+}
